754e8d0a0ac9525c3c5d5ca126d7c064ed9a0611
KshitizGupta04 <157566554+KshitizGupta04@users.noreply.github.com>
2026-03-22 14:51:54 +0530

    product dynamic

--- mini app/project inventory dashboard/script.js
+++ mini app/project inventory dashboard/script.js
@@ -6,5 +6,31 @@ let products = [
   { id: 5, name: "Shoes", price: 3000, stock: 3, category: "clothing" },
   { id: 6, name: "Watch", price: 2500, stock: 8, category: "accessories" },
   { id: 7, name: "Bag", price: 1500, stock: 6, category: "accessories" },
-  { id: 8, name: "Notebook", price: 100, stock: 20, category: "books" }
-];
+  { id: 8, name: "Notebook", price: 100, stock: 20, category: "books" }]
+
+  function renderProducts(productList) {
+  const container = document.getElementById("productGrid");
+  container.innerHTML = "";
+
+  if (productList.length === 0) {
+    container.innerHTML = "<p>No products found</p>";
+    return;
+  }
+
+  productList.forEach(product => {
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+      <h3>${product.name}</h3>
+      <p>Category: ${product.category}</p>
+      <p>Price: ₹${product.price}</p>
+      <p>Stock: ${product.stock}</p>
+      <button onclick="deleteProduct(${product.id})">Delete</button>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+// Initial render
+renderProducts(products);
