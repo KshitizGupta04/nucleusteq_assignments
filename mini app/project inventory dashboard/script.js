8d430b908d2be5b25e1d8eeb726c203b4151a980
KshitizGupta04 <157566554+KshitizGupta04@users.noreply.github.com>
2026-03-22 14:42:32 +0530

    project structure

---
+++ mini app/project inventory dashboard/script.js
@@ -0,0 +1,5 @@
+let products = [
+  { id: 1, name: "Laptop", price: 55000, stock: 5, category: "electronics" },
+  { id: 2, name: "Shirt", price: 1200, stock: 10, category: "clothing" },
+  { id: 3, name: "Book", price: 500, stock: 2, category: "books" }
+];
