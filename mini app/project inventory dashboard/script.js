84c670d648c5afe31bd39178305f9b719b90e85c
KshitizGupta04 <157566554+KshitizGupta04@users.noreply.github.com>
2026-03-22 14:49:57 +0530

    inital product data

--- mini app/project inventory dashboard/script.js
+++ mini app/project inventory dashboard/script.js
@@ -1,5 +1,10 @@
 let products = [
   { id: 1, name: "Laptop", price: 55000, stock: 5, category: "electronics" },
   { id: 2, name: "Shirt", price: 1200, stock: 10, category: "clothing" },
-  { id: 3, name: "Book", price: 500, stock: 2, category: "books" }
+  { id: 3, name: "Book", price: 500, stock: 2, category: "books" },
+  { id: 4, name: "Headphones", price: 2000, stock: 0, category: "electronics" },
+  { id: 5, name: "Shoes", price: 3000, stock: 3, category: "clothing" },
+  { id: 6, name: "Watch", price: 2500, stock: 8, category: "accessories" },
+  { id: 7, name: "Bag", price: 1500, stock: 6, category: "accessories" },
+  { id: 8, name: "Notebook", price: 100, stock: 20, category: "books" }
 ];
